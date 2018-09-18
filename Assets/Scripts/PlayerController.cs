cc629dbb8536a7979181a44a24d5ba1da085e15b
DavidJB526 <david.beaudoin@loop.colum.edu>
2018-09-18 08:35:54 -0500

    Add Test Level and Horizontal Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float playerSpeed, jumpForceForward, jumpForceUp;
+    
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float playerJump = Input.GetAxis("Jump");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        Vector3 jump = new Vector3(jumpForceForward, playerJump * jumpForceUp, 0.0f);
+
+        transform.position = transform.position + (movement * playerSpeed);
+        rb.AddForce(jump);
+
+    }
+}
