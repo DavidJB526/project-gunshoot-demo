61318b48c064e39aac5499ce8ec2ef3da0ee8602
David Beaudoin <David.Beaudoin@iam.local>
2018-09-25 13:58:10 -0500

    Add More Platforms

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject player;
+
+    private Vector3 offset;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        transform.position = player.transform.position + offset;
 	}
 }
