6d53364fc61b0af3739a4c4570a720647064f095
DavidJB526 <david.beaudoin@loop.colum.edu>
2018-11-19 12:54:55 -0600

    Re-adding Scripts for Code Review

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    [SerializeField]
+    private string nextScene;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && Input.GetButtonDown("Activate"))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/MouseTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseTracker : MonoBehaviour {
+
+    private Vector3 mousePosition;
+
+    [SerializeField]
+    private Vector3 firePoint;
+    [SerializeField]
+    private float angle;
+
+    private void FixedUpdate()
+    {
+        mousePosition = Input.mousePosition;
+        firePoint = Camera.main.WorldToScreenPoint(transform.position);
+        mousePosition.x = mousePosition.x - firePoint.x;
+        mousePosition.y = mousePosition.y - firePoint.y;
+        angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+}
