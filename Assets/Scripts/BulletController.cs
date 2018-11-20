e7570c92983155f50e5bba3a84ecc56c2a394ec1
David Beaudoin <David.Beaudoin@iam.local>
2018-11-20 12:52:22 -0600

    Specified Variables in BulletController OnTriggerEnter2D

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour 
 {
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag != "Player" && collision.tag != "Background" && collision.tag != "Collectable" && collision.tag != "Checkpoint")
+        bool isNotPlayerOrBackground = collision.tag != "Player" && collision.tag != "Background";
+        bool isNotCollectableOrCheckpoint = collision.tag != "Collectable" && collision.tag != "Checkpoint";
+
+        if (isNotCollectableOrCheckpoint && isNotPlayerOrBackground)
         {
             Destroy(gameObject);
         }
